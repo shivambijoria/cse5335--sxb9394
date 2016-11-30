1f5a7bd064afd71e425c0f08a83d1586cfec968b
shivam bijoria <shivambijoriya@gmail.com>
2016-11-29 19:03:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,27 +7,12 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-
-app.use(function(req, res, next) 
-{ 
-res.header('Access-Control-Allow-Origin', "*"); 
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); 
-res.header('Access-Control-Allow-Headers', 'Content-Type'); next();
-})
+var mongodb = require('mongodb');
 
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://shivambijoria:balloons1@ds113628.mlab.com:13628/heroku_xq8w25z3';
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
 
 var data_to_send=[
 	
@@ -135,78 +120,79 @@ var data_to_send=[
     
 	];
 
-var count=0;
+
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+
+    console.log('Connection established to', url);
+
+    collection = db.collection('country');
+	
+	collection.drop();
+
+    
+
+for(k=0;k<100;k++){
+    collection.insert(data_to_send[k], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+});
+  }
+ 
+  
+  
+  }
+});
+
+app.use(function(req, res, next) 
+{ 
+res.header('Access-Control-Allow-Origin', "*"); 
+res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); 
+res.header('Access-Control-Allow-Headers', 'Content-Type'); next();
+})
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+var count=1;
 	
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/api', function(req, res) {
-    if (count==100){
-		count=0;
+    if (count>100){
+		count=1;
 	}
-	res.json(data_to_send[count]);
-	count=count+1;	
+	collection.find({Id: count}).toArray(function (err, result) { 
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        console.log('Found:', result[0].Country);
+		res.json(result[0]);
+	count=count+1;
+      } else {
+        console.log('No document(s) found with defined "find" criteria!');
+      }
+      //Close connection
+      
+    });
+		
 });
 
-router.get('/', function(req, res) {
-	res.sendfile(path.join(__dirname + '/index.html'));
-	
-});
-
-/*
-router.get('/chart', function(req, res) {
-    res.json(
-	
-	[
-	
-            {"Country":"China","Population":1367485388},
-            {"Country":"India","Population":1251695584},
-			{"Country":"USA","Population":321368864},
-            {"Country":"Indonesia","Population":255993674},
-			
-			{"Country":"Brazil","Population":204259812},
-            {"Country":"Pakistan","Population":199085847},
-			{"Country":"Nigeria","Population":181562056},
-            {"Country":"Bangladesh","Population":168957745},
-			
-			{"Country":"Russia","Population":142423773},
-            {"Country":"Japan","Population":126919659},
-    
-	]
-	
-	);   
-});
-
-
-router.get('/map', function(req, res) {
-    res.json(
-	{"location":
-	[
-		{"lat":34.5553 ,"lng":69.2075},
-        {"lat":-36.8485 ,"lng":174.7633},
-		{"lat":35.6895 ,"lng":139.6917},
-		
-		{"lat":-12.6895 ,"lng":19.6917},
-		{"lat":43.6895 ,"lng":-41.6917},
-		{"lat":-65.6895 ,"lng":-119.6917},
-		
-		{"lat":12.6895 ,"lng":-19.6917},
-		{"lat":-43.6895 ,"lng":41.6917},
-		{"lat":65.6895 ,"lng":119.6917},
-		
-		{"lat":-20.0000 ,"lng":-20.0000},
-		
-		
-	]
-	}
-	);   
-});
-*/
-
-
-router.get('/html', function(req, res) {
-     
-	var st='<button onclick="getdataanddisplay()">Send an HTTP GET request to get JSON data and display in table and list form</button><br><button onclick="getchartdata()">Send an HTTP GET request to get JSON chart data</button><br><button onclick="displaychart()">Display chart</button><br><button onclick="getmapdata()">Send an HTTP GET request to get lattitude and longitude</button><br><button onclick="displaymap()">Display map</button><br>';
-	res.send(st);
-});
 
 // more routes for our API will happen here
 
